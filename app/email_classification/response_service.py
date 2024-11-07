6a7021198ba2b74c6117d198de0aace3acb2974b
nickschneider <n.schneider@tum.de>
2024-11-07 10:01:54 +0100

    adjust to only connect via shared sec

--- app/email_classification/response_service.py
+++ app/email_classification/response_service.py
@@ -13,38 +13,14 @@ class ResponseService:
         self.token_url = f"{self.angelos_url}:{self.port}/api/token"
         self.session = requests.Session()
         self.api_key = config.ANGELOS_APP_API_KEY
-        self.token = None
-        self.headers = {
-            "Content-Type": "application/json"
-        }
+        self._set_authorization_header()
 
     def _set_authorization_header(self, token):
         """Helper function to set the authorization header."""
-        self.headers["Authorization"] = f"Bearer {token}"
-
-    def _request_token(self):
-        """Retrieve a new token using the API key."""
-        logging.info("Requesting new token from authentication server.")
-        headers = {"x-api-key": self.api_key}
-        response = self.session.post(self.token_url, headers=headers)
-        response.raise_for_status()
-        self.token = response.json().get("access_token")
-        if self.token:
-            self._set_authorization_header(self.token)
-            logging.info("Token retrieved and stored successfully.")
-        else:
-            logging.error("Failed to retrieve token.")
-            raise ValueError("Authentication token not found in response.")
-
-    def _ensure_token(self):
-        """Ensure that a valid token is available before making a request."""
-        if not self.token:
-            self._request_token()
+        self.headers = {"x-api-key": self.api_key, "Content-Type": "application/json"}
 
     def get_response(self, payload):
         """Send a request to the API endpoint with the given payload."""
-        self._ensure_token()  # Ensure token is set before making a request
-
         try:
             logging.info(f"Sending POST request to {self.api_url}")
             response = self.session.post(self.api_url, json=payload, headers=self.headers)
