a413a71d3a19177f2744a8dc0f8ce42548e9ae9a
nickschneider <n.schneider@tum.de>
2024-11-13 15:32:08 +0100

    check connection if timeout reconnect and try sending agian

--- app/email_service/email_sender.py
+++ app/email_service/email_sender.py
@@ -1,4 +1,5 @@
 import logging
+import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
@@ -10,7 +11,9 @@ class EmailSender:
         self.email_client = email_client
 
     def send_reply_email(self, original_email: EmailDTO, reply_body):
-        if not self.email_client.smtp_connection:
+        smtp_conn = self.email_client.get_smtp_connection()
+
+        if not smtp_conn:
             self.email_client.connect_smtp()
 
         # Extract headers from the original email
@@ -29,5 +32,12 @@ class EmailSender:
         # Attach the reply body
         msg.attach(MIMEText(reply_body, 'plain'))
 
-        self.email_client.smtp_connection.send_message(msg)
-        logging.info("Reply email sent to %s", from_address)
+        try:
+            smtp_conn.send_message(msg)
+            logging.info("Reply email sent to %s", from_address)
+        except (smtplib.SMTPException, ConnectionError) as e:
+            logging.error("Failed to send email due to %s. Attempting to reconnect and resend.", e)
+            self.email_client.connect_smtp()
+            smtp_conn = self.email_client.get_smtp_connection()
+            smtp_conn.send_message(msg)
+            logging.info("Reply email re-sent to %s after reconnection", from_address)
